fc45a136814281fc0d3e59badd75c7482fb43384
Kevin Sun <kevsun1@gmail.com>
2023-10-30 18:26:39 -0700

    add BlogTest route and link

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Blog Test</title>
+	<meta name="description" content="Blog Test" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Dis is da blog</h1>
+
+	<p>
+		Coming soon.
+	</p>
+</div>
