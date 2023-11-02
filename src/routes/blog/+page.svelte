abf05ed8dc0e029e3b8374bc01923902d088a2dc
Kevin Sun <kevsun1@gmail.com>
2023-11-01 21:34:03 -0700

    update base styles to get dark mode theme

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -3,7 +3,7 @@
 	<meta name="description" content="Blog Test" />
 </svelte:head>
 
-	<h1>𐩳𐩢𐩺</h1>
+<h1>𐩳𐩢𐩺</h1>
 
 <div class="grid">
 	<div class="card"><h1>𐩳</h1></div>
@@ -16,7 +16,7 @@
 		width: 100%;
 		display: grid;
 		grid-template-columns: repeat(3, 1fr);
-		grid-gap: 8px;
+		grid-gap: 12px;
 	}
 
 	.card {
