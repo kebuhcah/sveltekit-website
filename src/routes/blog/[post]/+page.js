f2214473d10c4d8ceb9d647a072b9b8e15d40d2d
Kevin Sun <kevsun1@gmail.com>
2023-11-20 19:11:53 -0800

    get example blogpost to display correctly!

--- src/routes/blog/[post]/+page.js
+++ src/routes/blog/[post]/+page.js
@@ -2,13 +2,17 @@ import { error } from '@sveltejs/kit'
 
 export const load = async ({ params }) => {
     try {
+        console.log("TEST!")
+        console.log(params)
         const post = await import(`../../../lib/posts/${params.post}.md`)
+        console.log(post)
 
         return {
             PostContent: post.default,
             meta: { ...post.metadata, slug: params.post }
         }
     } catch (err) {
+        // @ts-ignore
         throw error(404, err)
     }
 }
