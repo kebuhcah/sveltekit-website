3780352a93974a6fc9e1d26a97f70e7b78b09073
Kevin Sun <kevsun1@gmail.com>
2024-01-02 19:08:33 -0800

    extremely basic attempt at venn diagram

---
+++ src/routes/venn/+page.svelte
@@ -0,0 +1,30 @@
+<div class="text-column">
+	<div class="venn-circle top">𐩺</div>
+	<div class="venn-circle left">𐩢</div>
+	<div class="venn-circle right">𐩳</div>
+</div>
+
+<style>
+	.venn-circle {
+		font-size: 4rem;
+		border-style: solid;
+		border-radius: 50%;
+        height: 300px;
+        width: 300px;
+        position: fixed;
+	}
+
+    .top {
+
+    }
+
+    .left {
+        margin-top: 300px;
+        margin-left: -88px
+    }
+
+    .right {
+        margin-top: 300px;
+        margin-left: 88px;
+    }
+</style>
