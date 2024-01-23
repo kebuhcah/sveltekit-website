5f9484aeaeee72163d087aec4e46240c996189ad
Kevin Sun <kevsun1@gmail.com>
2024-01-22 21:56:25 -0800

    Change PostsList to flexbox

--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -3,11 +3,12 @@
 	 * @type {any[]}
 	 */
 	 export let posts = [];
+	 console.log(posts);
 </script>
 
-<ul class="posts-list">
+<div class="posts-list">
 	{#each posts as post}
-		<li>
+		<div class="post-card">
 			<article>
 				<a href="/blog/{post.slug}">
 					<!--<img
@@ -22,8 +23,43 @@
 					</h2>
 				</a>
 			</article>
-
 			<p>{post.excerpt}</p>
-		</li>
+			<div class="categories">
+				{#each post.categories as category}
+					<span class="category">
+						<a href="/blog/category/{category}/">
+							{category}
+						</a>
+					</span>
+				{/each}
+			</div>
+		</div>
 	{/each}
-</ul>
+	</div>
+
+<style>
+	.posts-list {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+	}
+
+	.post-card {
+		border-style: solid;
+		border-width: 1px;
+		border-color: white;
+
+		min-width: 16rem;
+		max-width: 24rem;
+
+		margin: 0.5rem;
+		padding: 0rem 1.5rem;
+	}
+
+	.categories {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: left;
+		margin-bottom: 0.5rem;
+	}
+</style>
