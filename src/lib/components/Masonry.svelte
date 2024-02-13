c23e408416acab30eb19d539bdf33c60c7e6f4ea
Kevin Sun <kevsun1@gmail.com>
2024-02-12 18:48:47 -0800

    Remove unused styling (moved to sub-component)

--- src/lib/components/Masonry.svelte
+++ src/lib/components/Masonry.svelte
@@ -59,11 +59,4 @@
         flex-direction: column;
         width: 33%
     }
-
-    .grid-item {
-        border-width: 1px;
-        border-style: solid;
-        margin: 0.5rem;
-        padding: 0.5rem;
-    }
 </style>
