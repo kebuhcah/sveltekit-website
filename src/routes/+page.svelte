7541c05695bdf71d8f69041e741198dddee6f0aa
Kevin Sun <kevsun1@gmail.com>
2024-04-07 21:21:06 -0700

    Title styling tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,16 +11,21 @@
 </svelte:head>
 
 <h1 class="title">𐩳𐩢𐩺</h1>
-<h1 class="title">Sun Language Theories</h1>
-<h2 class="title">by Kevin Fei Sun</h2>
+<h1 class="title">𝕾𝖚𝖓 𝕷𝖆𝖓𝖌𝖚𝖆𝖌𝖊 𝕿𝖍𝖊𝖔𝖗𝖎𝖊𝖘</h1>
+<h2 class="title">by 𝕂𝕖𝕧𝕚𝕟 𝔽𝕖𝕚 𝕊𝕦𝕟</h2>
 
 <PostsList posts={data.posts} />
 
 <Pagination currentPage={1} totalPosts={data.total} />
 
 <style>
-	.title {
+	h1.title {
 		text-align: center;
 		margin: 0.2rem;
+	}
+
+	h2.title {
+		text-align: center;
+		margin: 1rem;
 	}
 </style>
