9cadf720a4e87fb59f088d71cf97d1d8d2bfc129
Kevin Sun <kevsun1@gmail.com>
2024-04-09 13:18:32 -0700

    Remove icon too

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,20 +10,10 @@
 	<meta name="description" content="Sun Language Theories" />
 </svelte:head>
 
-<h1 class="title">𐩳𐩢𐩺</h1>
-
 <PostsList posts={data.posts} />
 
 <Pagination currentPage={1} totalPosts={data.total} />
 
 <style>
-	h1.title {
-		text-align: center;
-		margin: 0.2rem;
-	}
 
-	h2.title {
-		text-align: center;
-		margin: 1rem;
-	}
 </style>
